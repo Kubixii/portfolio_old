d964c4a79fedee4adc516315c4bbc76f99149b91
Kubixii <39913230+Kubixii@users.noreply.github.com>
2022-04-02 17:11:09 +0200

    + meta tags, favicon, styling

--- src/components/links/link/Link.jsx
+++ src/components/links/link/Link.jsx
@@ -3,17 +3,16 @@ import './Link-mobile.css'
 
 import React from 'react';
 
-//TODO cannot figure out why mobile css is not affecting anything 
 const Link = ({ name, imageURL, URL }) => {
     return (
-        <a href={URL} target="blank">
-            <div className='link'>
+        <div className='link'>
+            <a href={URL} target="blank">
                 <div className='linkImageWrapper'>
                     <img src={imageURL} alt={imageURL} />
                 </div>
                 <p>{name}</p>
-            </div>
-        </a>
+            </a>
+        </div>
     );
 }
 
